fb2a6fc6c89a1be82d90b991628b41eee49f190c
mikhailidi <slipknot1.ru>
2017-07-09 13:03:47 +0200

    Updated some files

---
+++ resources/views/layouts/blog/partials/_head.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog | @yield('title')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+@yield('style')
+
+<!-- Custom styles for this template -->
+    <link href="css/blog.css" rel="stylesheet">
+
+</head>
